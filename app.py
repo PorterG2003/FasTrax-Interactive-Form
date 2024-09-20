4ac806162b02a63ae79d33d9e85713dea47aed91
PorterG2003 <porterg2003@gmail.com>
2024-09-20 12:36:34 -0600

    Publish

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, request, jsonify, render_template
 from fastrax_handler import FasTrax
+import boto3
+import json
+import logging
+import os
 
 app = Flask(__name__)
 
+ROUTE = os.getenv('ROUTE', '')
+
+# Initialize the S3 client
+s3 = boto3.client('s3', region_name='us-west-2')  # Specify your region here)
+BUCKET_NAME = 'zappa-vy4tv2kgd' 
+
+boto3.set_stream_logger(name='boto3', level=logging.DEBUG)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', route=ROUTE, submitted_data="")
 
 @app.route('/submit', methods=['POST'])
 def submit_form():
@@ -16,7 +28,7 @@ def submit_form():
         return jsonify({'message': 'No JSON body provided'}), 400
 
     # Initialize FasTrax with the request body
-    site_handler = FasTrax(body)
+    site_handler = FasTrax(body, route=ROUTE)
 
     # Run the FasTrax handler and get status and message
     status, message = site_handler.run()
@@ -24,8 +36,43 @@ def submit_form():
     # Return the response as a JSON object with the appropriate headers
     return jsonify({
         'message': message
-    }), status, {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-    }
+    }), status
+
+@app.route('/submitted-forms/<uuid>', methods=['GET'])
+def load_submitted_form(uuid):
+    # The S3 key where the cookie data is stored
+    s3_key = f'cookies/{uuid}.json'
+
+    try:
+        # Fetch the data from S3
+        s3_object = s3.get_object(Bucket=BUCKET_NAME, Key=s3_key)
+        cookie_data = json.loads(s3_object['Body'].read().decode('utf-8'))
+        cookie_data = "{}" if not cookie_data else cookie_data
+
+        # Render the index.html template with the cookie data
+        return render_template('index.html', route=ROUTE, submitted_data=cookie_data)
+
+    except Exception as e:
+        # If there was an issue fetching the data, return an error message
+        return jsonify({'message': f"Error fetching data for UUID {uuid} from bucket {BUCKET_NAME}: {str(e)}"}), 404
+
+if __name__ == '__main__': # LOCAL RUNS
+    app.run(debug=True)
+
+    # Configure the root logger
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[logging.StreamHandler()]
+    )
+    logger = logging.getLogger(__name__)
+
+else: # AWS RUNS
+    LOCAL = False
+    TEST = False
+
+    # Configure the root logger
+    logger = logging.getLogger()
+    logger.setLevel("INFO")
+
+    # AWS CALLS LAMBDA HANDLER ITSELF
--- fastrax_handler.py
+++ fastrax_handler.py
@@ -1,5 +1,7 @@
 import logging
 import boto3 #type: ignore
+import uuid
+import json
 
 from email_handler import EmailSender
 from helpers import get_secret
@@ -7,13 +9,18 @@ from helpers import get_secret
 logger = logging.getLogger(__name__)
 
 class FasTrax:
-    def __init__(self, body):
+    def __init__(self, body, route="/dev"):
         self.body = body
 
         self.email_sender = self.get_email_sender()
 
         self.form_fields = body.get('FormFields', {})
         self.form = body.get('Form', '')
+
+        # Initialize the S3 client
+        self.s3 = boto3.client('s3')
+        self.bucket_name = 'zappa-vy4tv2kgd'  # Replace with your S3 bucket name
+        self.route = route
 
     def get_email_sender(self):
         smtp_secret = get_secret('smtp')
@@ -32,6 +39,10 @@ class FasTrax:
             return 400, f"No form in the FasTrax handler matches the received form, '{self.form}'"
 
     def runSubmit(self):
+        # Save cookie data to s3
+        cookie_data = self.body.get('cookieData')
+        unique_id = self.save_cookie_data_to_s3(cookie_data)
+
         # Prep email content
         intro = "<p>You have a new interactive form submission!</p>"
         content_lines = []
@@ -47,24 +58,49 @@ class FasTrax:
                         content_lines.append(f"&emsp;&emsp;{sub_key}: {sub_value}<br>")
 
         content = "".join(content_lines)
+
+        if self.route == "/dev":
+            base_url = f"https://86whlxedc6.execute-api.us-west-2.amazonaws.com{self.route}"
+        elif self.route == "/stage":
+            base_url = f"https://8qjlq1wt87.execute-api.us-west-2.amazonaws.com{self.route}"
+        elif self.route == "":
+            base_url = f"https://form.fastraxpermitservice.com"
+
         
         # Complete HTML email body
         html_content = f"""
         <html>
         <body>
             {intro}
+            <p>Check it out at {base_url}/submitted-forms/{unique_id}</p><br>
             <p>{content}</p>
         </body>
         </html>
         """
 
+        unit_numbers = [str(self.form_fields['Section 5'][unit]["Unit Number"]) for unit in self.form_fields['Section 5'].keys()]
+        subject = f"{self.form_fields['Section 1']['Carrier']}"
+        subject += " - Unit Numbers: " if len(unit_numbers) > 1 else " - Unit Number: "
+        for index, unit_number in enumerate(unit_numbers):
+            subject += unit_number
+            if index+1 != len(unit_numbers):
+                subject += ", "
+
+
         # Send notification emails
-        recipients = ["porter@pagtechnologies.com"]#, "ap@fastraxpermitservice.com "]
+        if self.route=="/stage":
+            recipients = ["porter@pagtechnologies.com", "ap@fastraxpermitservice.com"]
+        elif self.route=="/":
+            recipients = ["porter@pagtechnologies.com", "ap@fastraxpermitservice.com"]
+        else:
+            recipients = ["porter@pagtechnologies.com"]
+            subject += "THIS WAS ONLY SENT TO THIS EMAIL"
+
         for recipient in recipients:
             self.email_sender.send_email(
-                from_address="porter@pagtechnologies.com", 
+                from_address="porter@pagtechnologies.com",
                 to_address=recipient,
-                subject="Interactive Form Submission",
+                subject=subject,
                 body=html_content,
                 attachment_path=None,
                 is_html=True  # Assuming send_email method can take this parameter to set content type
@@ -72,3 +108,31 @@ class FasTrax:
 
         # Return response
         return 200, 'Form data processed successfully!'
+
+    def save_cookie_data_to_s3(self, cookie_data):
+        """
+        Save the provided cookie data to an S3 bucket with a unique identifier.
+        :param cookie_data: Dictionary containing the cookie data to be saved.
+        :return: The unique identifier used to save the data in S3.
+        """
+        # Generate a unique identifier
+        unique_id = str(uuid.uuid4())
+
+        # Convert the cookie data to a JSON string
+        cookie_data_string = json.dumps(cookie_data)
+
+        # The key (filename) for the S3 object
+        s3_key = f'cookies/{unique_id}.json'
+
+        try:
+            # Save the JSON string to the S3 bucket
+            self.s3.put_object(Bucket=self.bucket_name, Key=s3_key, Body=cookie_data_string)
+
+            logger.info(f"Cookie data saved to S3 with key: {s3_key}")
+
+            # Return the unique identifier
+            return unique_id
+
+        except Exception as e:
+            logger.error(f"Failed to save cookie data to S3: {str(e)}")
+            raise e
